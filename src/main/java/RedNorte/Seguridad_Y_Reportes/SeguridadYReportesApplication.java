681620ff2d668831a2a8b0fb833b637a0479fc76
Benjamin Avila <ben.avilar@duocuc.cl>
2026-06-11 22:53:04 -0400

    Creacion de Microservicio

---
+++ src/main/java/RedNorte/Seguridad_Y_Reportes/SeguridadYReportesApplication.java
@@ -0,0 +1,13 @@
+package RedNorte.Seguridad_Y_Reportes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeguridadYReportesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeguridadYReportesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/RedNorte/Seguridad_Y_Reportes/SeguridadYReportesApplicationTests.java
@@ -0,0 +1,13 @@
+package RedNorte.Seguridad_Y_Reportes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeguridadYReportesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
